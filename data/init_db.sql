0abf216d7dc9df2a46716f271dc6dbf3ce3656dd
id2835 <madie.perrin-beau@eleve.ensai.fr>
2026-08-28 13:47:57 +0000

    correction data

--- data/init_db.sql
+++ data/init_db.sql
@@ -1,10 +1,18 @@
 -----------------------------------------------------
--- Information code
+-- Utilisateur
 -----------------------------------------------------
-DROP TABLE IF EXISTS simulation_code CASCADE;
-CREATE TABLE simulation_code (
+DROP TABLE IF EXISTS user CASCADE;
+CREATE TABLE user (
+    id_user    SERIAL PRIMARY KEY,
+    username     VARCHAR(30) UNIQUE,
+    password     VARCHAR(256),
+    email        VARCHAR(50),
+    access_token VARCHAR(255)
+);
+
+'''
     id_code    SERIAL PRIMARY KEY,
     cle_HMAC    VARCHAR,
     lt_package    ARRAY,
     empreinte    INTEGER
-);
+'''
---
+++ data/pop_db.sql
@@ -0,0 +1,8 @@
+INSERT INTO user(username, password, email) VALUES
+('admin',     '0000',  'admin@project.io'),
+('a',         'a',     'a@ensai.fr'),
+('maurice',   '1234',  'maurice@ensai.fr'),
+('batricia',  '9876',  'bat@project.io'),
+('miguel',    'abcd',  'miguel@project.io'),
+('gilbert',   'toto',  'gilbert@project.io'),
+('junior',    'aaaa',  'junior@project.io');
