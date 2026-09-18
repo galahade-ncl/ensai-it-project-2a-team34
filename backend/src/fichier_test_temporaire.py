efb617bdfbbe4bf776efa7e3cb70d2d83562ef87
id2835 <madie.perrin-beau@eleve.ensai.fr>
2026-09-18 20:41:15 +0000

    correction fichier_test

--- backend/src/fichier_test_temporaire.py
+++ backend/src/fichier_test_temporaire.py
@@ -1,9 +1,9 @@
 from business_object.qualitygate import QualityGate
 
 # Initialization
-#initialize_logs("Webservice")
-#load_environment_variables()
-#display_values()
+# initialize_logs("Webservice")
+# load_environment_variables()
+# display_values()
 
 qualitygate = QualityGate(2, 0, 50, "Good")
 print(qualitygate)
