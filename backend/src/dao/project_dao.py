4439564ff5c106dc9b1aa51dc352a59f20beb11f
galahade <galahade.nicolie@eleve.ensai.fr>
2026-09-25 13:02:11 +0000

    fix bug

--- backend/src/dao/project_dao.py
+++
@@ -1,6 +0,0 @@
-from business_object.project import P
-from dao.db_connection import DBConnection
-from utils.log_utils import get_logger, log
-from utils.singleton import Singleton
-
-logger = get_logger(__name__)
